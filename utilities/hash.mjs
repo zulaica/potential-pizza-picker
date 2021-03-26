c2cbf04d6ea7de31632d714653eef1fde025d04c
David Zulaica <zulaica@gmail.com>
2021-03-26 16:29:46 -0700

    Simplify hashing function

--- utilities/hash.mjs
+++ utilities/hash.mjs
@@ -1,9 +1,7 @@
 const stringToHash = (string, offset = 1) => {
-  return (
-    string.split("").reduce((hash, currentLetter) => {
-      return hash * offset + currentLetter.charCodeAt(0);
-    }, 0) >>> 0
-  );
+  return string.split("").reduce((hash, currentLetter) => {
+    return hash * offset + currentLetter.charCodeAt(0);
+  }, 0);
 };
 
 export default stringToHash;
