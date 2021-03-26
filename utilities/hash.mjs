ad0e269f7e549476aaafc191f6b5209c32c7739d
David Zulaica <zulaica@gmail.com>
2021-03-26 13:52:56 -0700

    Add VERY simple hashing function

---
+++ utilities/hash.mjs
@@ -0,0 +1,9 @@
+const stringToHash = (string, offset = 1) => {
+  return (
+    string.split("").reduce((hash, currentLetter) => {
+      return hash * offset + currentLetter.charCodeAt(0);
+    }, 0) >>> 0
+  );
+};
+
+export default stringToHash;
