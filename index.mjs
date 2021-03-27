9f85ba981c29071f5c8dee91a88178b86e1699dd
David Zulaica <zulaica@gmail.com>
2021-03-26 18:23:23 -0700

    Increase filter size and update logs to reflect number and logic of hashing functions

--- index.mjs
+++ index.mjs
@@ -1,20 +1,82 @@
 import stringToHash from "./utilities/hash.mjs";
 
-const BIT_LENGTH = 25;
-const OFFSET_A = 32;
-const OFFSET_B = 64;
+const BIT_LENGTH = 217;
+const OFFSET = 32;
 
 console.info("Pepperoni:");
-console.info(stringToHash("Pepperoni") % BIT_LENGTH);
-console.info(stringToHash("Pepperoni", OFFSET_A) % BIT_LENGTH);
-console.info(stringToHash("Pepperoni", OFFSET_B) % BIT_LENGTH);
-console.info("---");
-console.info("Jalapeños:");
-console.info(stringToHash("Jalapeños") % BIT_LENGTH);
-console.info(stringToHash("Jalapeños", OFFSET_A) % BIT_LENGTH);
-console.info(stringToHash("Jalapeños", OFFSET_B) % BIT_LENGTH);
-console.info("---");
+console.info(
+  (stringToHash("Pepperoni") + stringToHash("Pepperoni", OFFSET) * 1) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Pepperoni") + stringToHash("Pepperoni", OFFSET) * 2) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Pepperoni") + stringToHash("Pepperoni", OFFSET) * 3) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Pepperoni") + stringToHash("Pepperoni", OFFSET) * 4) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Pepperoni") + stringToHash("Pepperoni", OFFSET) * 5) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Pepperoni") + stringToHash("Pepperoni", OFFSET) * 6) %
+    BIT_LENGTH
+);
+
 console.info("Mushrooms:");
-console.info(stringToHash("Mushrooms") % BIT_LENGTH);
-console.info(stringToHash("Mushrooms", OFFSET_A) % BIT_LENGTH);
-console.info(stringToHash("Mushrooms", OFFSET_B) % BIT_LENGTH);
+console.info(
+  (stringToHash("Mushrooms") + stringToHash("Mushrooms", OFFSET) * 1) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Mushrooms") + stringToHash("Mushrooms", OFFSET) * 2) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Mushrooms") + stringToHash("Mushrooms", OFFSET) * 3) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Mushrooms") + stringToHash("Mushrooms", OFFSET) * 4) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Mushrooms") + stringToHash("Mushrooms", OFFSET) * 5) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Mushrooms") + stringToHash("Mushrooms", OFFSET) * 6) %
+    BIT_LENGTH
+);
+
+console.info("Jalapeños:");
+console.info(
+  (stringToHash("Jalapeños") + stringToHash("Jalapeños", OFFSET) * 1) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Jalapeños") + stringToHash("Jalapeños", OFFSET) * 2) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Jalapeños") + stringToHash("Jalapeños", OFFSET) * 3) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Jalapeños") + stringToHash("Jalapeños", OFFSET) * 4) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Jalapeños") + stringToHash("Jalapeños", OFFSET) * 5) %
+    BIT_LENGTH
+);
+console.info(
+  (stringToHash("Jalapeños") + stringToHash("Jalapeños", OFFSET) * 6) %
+    BIT_LENGTH
+);
