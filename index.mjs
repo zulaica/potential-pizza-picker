0f2b5cc2a696185c3ec7495bd0088c1cece2427e
David Zulaica <zulaica@gmail.com>
2021-03-26 19:34:31 -0700

    Update function to accept more parameters with safe defaults

--- index.mjs
+++ index.mjs
@@ -1,21 +1,26 @@
 import stringToHash from "./utilities/hash.mjs";
 
-const BIT_LENGTH = 217;
+const VECTOR_LENGTH = 217;
 const OFFSET = 32;
 const HASH_COUNT = 6;
 
-const indexesFromHashes = (string, count) => {
+const indexesFromHashes = (
+  string,
+  hashCount = HASH_COUNT,
+  offset = OFFSET,
+  vectorLength = VECTOR_LENGTH
+) => {
   const indexes = [];
 
-  for (let i = 1; i <= count; i++) {
+  for (let i = 1; i <= hashCount; i++) {
     indexes.push(
-      (stringToHash(string) + stringToHash(string, OFFSET) * i) % BIT_LENGTH
+      (stringToHash(string) + stringToHash(string, offset) * i) % vectorLength
     );
   }
 
   return indexes;
 };
 
-console.info("Pepperoni: ", indexesFromHashes("Pepperoni", HASH_COUNT));
-console.info("Mushrooms: ", indexesFromHashes("Mushrooms", HASH_COUNT));
-console.info("Jalapeños:", indexesFromHashes("Jalapeños", HASH_COUNT));
+console.info("Pepperoni: ", indexesFromHashes("Pepperoni"));
+console.info("Mushrooms: ", indexesFromHashes("Mushrooms"));
+console.info("Jalapeños:", indexesFromHashes("Jalapeños"));
