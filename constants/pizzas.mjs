a7d3474109a995edcab510bf246768003a258d95
David Zulaica <zulaica@gmail.com>
2021-03-29 00:03:13 -0700

    Add more pizzas to menu

--- constants/pizzas.mjs
+++ constants/pizzas.mjs
@@ -1,12 +1,26 @@
 export const PIZZAS = Object.freeze([
   {
-    name: "The Adjective Noun",
+    name: "The Yosemite Sam",
     ingredients: [
       "Green Peppers",
       "Jalapeños",
       "Pepperoni",
       "Roasted Garlic",
       "Sausage",
+    ],
+  },
+  {
+    name: "The Porky Pig",
+    ingredients: ["Bacon", "Canadian Bacon", "Pepperoni", "Salami", "Sausage"],
+  },
+  {
+    name: "The Bugs Bunny",
+    ingredients: [
+      "Basil",
+      "Fresh Mozzarella",
+      "Ricotta",
+      "Spinach",
+      "Tomatoes",
     ],
   },
 ]);
