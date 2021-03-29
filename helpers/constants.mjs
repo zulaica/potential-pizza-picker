7286a1c5e3563e8c26740670822146981caf855d
David Zulaica <zulaica@gmail.com>
2021-03-28 22:35:49 -0700

    Add note about potentially replacing constants

--- helpers/constants.mjs
+++ helpers/constants.mjs
@@ -1,3 +1,6 @@
+// TODO: Add a function to calculate the optimal number of hashing functions
+// and bit vector length based on the number of potential items to be filtered
+// instead of manually managing these values.
 const CONSTANTS = Object.freeze({
   HASH_COUNT: 6,
   OFFSET: 32,
