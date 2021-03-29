3e9dc077053c7884fff11343b430fcf0769226d0
David Zulaica <zulaica@gmail.com>
2021-03-28 22:22:37 -0700

    Create BloomFilter class with `add` method

--- index.mjs
+++ index.mjs
@@ -20,6 +20,23 @@ const indexesFromHashes = (
   return indexes;
 };
 
-console.info("Pepperoni: ", indexesFromHashes("Pepperoni"));
-console.info("Mushrooms: ", indexesFromHashes("Mushrooms"));
-console.info("Jalapeños:", indexesFromHashes("Jalapeños"));
+const BloomFilter = class {
+  constructor(size) {
+    this.bits = [...Array(size).fill(0)];
+  }
+
+  add(string) {
+    const indexes = indexesFromHashes(string);
+
+    for (let i = 0; i < indexes.length; i++) {
+      this.bits[indexes[i]] = 1;
+    }
+
+    return this.bits;
+  }
+};
+
+const bf = new BloomFilter(VECTOR_LENGTH);
+
+console.info(bf.bits);
+console.info(bf.add("Pepperoni"));
