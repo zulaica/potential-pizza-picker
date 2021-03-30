55f78d6a2f1382cab478a6c1fb1fa29088b82da2
David Zulaica <zulaica@gmail.com>
2021-03-29 23:55:09 -0700

    Add another pizza

--- scripts/constants/pizzas.mjs
+++ scripts/constants/pizzas.mjs
@@ -1,3 +1,5 @@
+// TODO: This would be a lot easier with a function that dynamically creates x
+// unique pizzas, but it would also be less fun.
 export const PIZZAS = Object.freeze([
   {
     name: "The Yosemite Sam",
@@ -49,6 +51,16 @@ export const PIZZAS = Object.freeze([
       "Crushed Garlic",
       "Goat Cheese",
       "Jalapeños",
+      "Tomatoes",
+      "White Onions",
+    ],
+  },
+  {
+    name: "The Elmer Fudd",
+    ingredients: [
+      "Fresh Mozzarella",
+      "Meatballs",
+      "Ricotta",
       "Tomatoes",
       "White Onions",
     ],
